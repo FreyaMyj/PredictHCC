63608b48def237e3481282279f52aca64e85e7de
Arnold <arnoldchow@outlook.com>
2024-04-01 22:40:06 +0800

    Add class MLP

---
+++ Model/MLP.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.act = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.act(out)
+        out = self.fc2(out)
+        return out
